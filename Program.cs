455f2d8a22870632315834a9af068c50f241cbbb
KozlovOleg97 <kozlovoleg2021@gmail.com>
2022-10-30 23:30:08 +0300

    Creating AppCalculator using Algorithm - Binary Tree

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// sort out more complex examples
+
+using Expressions_For_Calculator;
+
+string s = "2+2";
+ExpressionTree e = ExpressionTree.Build(s);
+ExprVarValue[] vars = { new ExprVarValue("x", 2), new ExprVarValue("y", 3) };
+Console.WriteLine(s);
+foreach (ExprVarValue var in vars)
+    Console.WriteLine(var);
+Console.Write(e.ToString() + " = ");
+Console.WriteLine(e.getValue(vars));
+
+Console.ReadKey();
